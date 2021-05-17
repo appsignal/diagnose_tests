4ec0b71e0992f95f76310002d984a52e358bd66f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-05-17 11:29:35 +0200

    Add Runner

--- spec/diagnose_spec.rb
+++ spec/diagnose_spec.rb
@@ -1,33 +1,50 @@
+class Runner
+  def initialize
+    @read, @write = IO.pipe
+  end
+
+  def run
+    Dir.chdir(directory)
+    @pid = spawn(run_command, out: @write)
+  end
+
+  def readline
+    @read.readline
+  end
+
+  def stop
+    Process.kill(3, @pid)
+  end
+end
+
+class Runner::Ruby < Runner
+  def directory
+    File.join(__dir__, "../ruby")
+  end
+
+  def run_command
+    "bundle exec appsignal diagnose"
+  end
+end
+
 RSpec.describe "Diagnose" do
   before do
-    gemfile = File.join(__dir__, "../ruby/Gemfile")
-    command = "bundle exec --gemfile='#{gemfile}' appsignal diagnose"
-    puts command
-
-    @read, write = IO.pipe
-    @pid = spawn(command, out: write)
+    @runner = Runner::Ruby.new()
+    @runner.run()
   end
 
   it "prints the diagnose header" do
-    expect(next_line).to match(/AppSignal diagnose/)
-    expect(next_line).to match(/================================================================================/)
-    expect(next_line).to match(/Use this information to debug your configuration./)
+    expect(@runner.readline).to match(/AppSignal diagnose/)
+    expect(@runner.readline).to match(/================================================================================/)
+    expect(@runner.readline).to match(/Use this information to debug your configuration./)
 
-    expect(next_line).to match(/More information is available on the documentation site./)
-    expect(next_line).to match(%r(https://docs.appsignal.com/))
-    expect(next_line).to match(%r(Send this output to support@appsignal.com if you need help.))
-    expect(next_line).to match(/================================================================================/)
+    expect(@runner.readline).to match(/More information is available on the documentation site./)
+    expect(@runner.readline).to match(%r(https://docs.appsignal.com/))
+    expect(@runner.readline).to match(%r(Send this output to support@appsignal.com if you need help.))
+    expect(@runner.readline).to match(/================================================================================/)
   end
 
   after do
-    Process.kill(3, @pid)
-    @read.close
-  end
-
-  def next_line
-    begin
-      @read.readline
-    rescue IOError
-    end
+    @runner.stop
   end
 end
