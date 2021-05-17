5bf982752895f4405b3f83f1bdb7d3f3cd38eb9a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-05-17 12:21:03 +0200

    Add expect_output

--- spec/diagnose_spec.rb
+++ spec/diagnose_spec.rb
@@ -75,17 +75,24 @@ RSpec.describe "Diagnose" do
   end
 
   it "prints the diagnose header" do
-    expect(@runner.readline).to match(/AppSignal diagnose/)
-    expect(@runner.readline).to match(/================================================================================/)
-    expect(@runner.readline).to match(/Use this information to debug your configuration./)
-
-    expect(@runner.readline).to match(/More information is available on the documentation site./)
-    expect(@runner.readline).to match(%r(https://docs.appsignal.com/))
-    expect(@runner.readline).to match(%r(Send this output to support@appsignal.com if you need help.))
-    expect(@runner.readline).to match(/================================================================================/)
+    expect_output([
+      %r(AppSignal diagnose),
+      %r(================================================================================),
+      %r(Use this information to debug your configuration.),
+      %r(More information is available on the documentation site.),
+      %r(https://docs.appsignal.com/),
+      %r(Send this output to support@appsignal.com if you need help.),
+      %r(================================================================================)
+    ])
   end
 
   after do
     @runner.stop
+  end
+
+  def expect_output(expected)
+    expected.each do |line|
+      expect(@runner.readline).to match(line)
+    end
   end
 end
