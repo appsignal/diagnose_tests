3e48e26a1ce6585634e16676642a83c46c5a73a5
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-05-18 09:34:47 +0200

    Expect "yes" for Extension loaded

--- spec/diagnose_spec.rb
+++ spec/diagnose_spec.rb
@@ -129,7 +129,7 @@ RSpec.describe "Diagnose" do
       %r(  Language: #{@runner.language_name}),
       %r(  (Gem|Package) version: \d+\.\d+\.\d+),
       %r(  Agent version: \w{6}),
-      %r(  (Extension|Nif) loaded: true)
+      %r(  (Extension|Nif) loaded: yes)
     ])
   end
 
