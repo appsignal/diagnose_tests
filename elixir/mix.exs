8075e28a6398bf364c8e6892119e542d1276791b
Tom de Bruijn <tom@tomdebruijn.com>
2021-10-22 11:31:21 +0200

    Update Elixir app to run on older versions
    Our Elixir test suite tests until version 1.9.0, so let's make sure we
    can test it on that version as well.

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule Elixir.MixProject do
     [
       app: :elixir,
       version: "0.1.0",
-      elixir: "~> 1.11.0",
+      elixir: ">= 1.9.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
